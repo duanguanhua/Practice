86a69601cc0be67da8c7bccf6a5e2cb606ca9617
DuanGuanhua <1171193009@fafu.edu.cn>
2017-12-27 11:14:00 +0800

    day31

---
+++ src/JavaSE/day31_gjp/app/MainApp.java
@@ -0,0 +1,12 @@
+package JavaSE.day31_gjp.app;
+
+import JavaSE.day31_gjp.view.MainView;
+
+/*
+ *  主程序类,作用,开启软件程序
+ */
+public class MainApp {
+    public static void main(String[] args) {
+        new MainView().run();
+    }
+}
